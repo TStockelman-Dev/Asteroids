8b203b690a1460c0130d2045f1471c63643e7a10
Travis Stockelman <tstockelman.dev@gmail.com>
2026-05-11 21:23:02 -0400

    implemented player rotation

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = pygame.time.Clock().tick(60) / 1000
